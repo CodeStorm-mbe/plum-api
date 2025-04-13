cd4545e79f63393be611a38e49d34084afca137a
Emmanuel <emmanuelscre1@gmail.com>
2025-04-13 13:25:23 +0100

    Configurer REST_FRAMEWORK pour drf_yasg

--- plum_project/settings.py
+++ plum_project/settings.py
@@ -201,7 +201,8 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
         'user': '1000/day'
-    }
+    },
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
 
 # JWT settings
